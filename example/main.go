aac80c12ca743d048504144a636fe220451ccc46
liuchao02 <qq15810595175>
2019-10-31 17:22:13 +0800

    结构调整

--- example/main.go
+++ example/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/Tor-Nom/time_pro/service"
+	"github.com/Tor-Nom/time_pro"
 )
 
 func main() {
 
 	time := "2019-10-11 00:00:00"
 
-	beginTime, endTime := service.GetTime(time)
+	beginTime, endTime := time_pro.GetTime(time)
 	fmt.Println("beginTime => ", beginTime)
 	fmt.Println("endTime => ", endTime)
 }
--- time_pro.go
+++ time_pro.go
@@ -1,4 +1,4 @@
-package service
+package time_pro
 
 import "time"
 
